532a0bee87fcb913663e584f7609437a5cacece0
Jerri Magnanti <juniormagnanti@gmail.com>
2022-08-06 00:04:18 -0400

    backend

---
+++ backend/src/main/java/com/tonejerri/tonejerri/TonejerriApplication.java
@@ -0,0 +1,13 @@
+package com.tonejerri.tonejerri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonejerriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonejerriApplication.class, args);
+	}
+
+}
