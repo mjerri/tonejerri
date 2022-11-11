dc1ff42b5810325e3d86661e80ddd278fc4ac441
Jerri Magnanti <juniormagnanti@gmail.com>
2022-11-10 22:48:34 -0400

    spring boot 2.7.5

---
+++ backend/src/test/java/com/tonejerri/tonejerri/TonejerriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonejerri.tonejerri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TonejerriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
